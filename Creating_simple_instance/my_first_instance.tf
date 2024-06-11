9f6aaa9a139e83714d4c129169b841d352eb7645
distinto1 <ugot4daniel@gmail.com>
2024-06-11 15:23:02 +0100

    updated lines

--- Creating_simple_instance/my_first_instance.tf
+++ Creating_simple_instance/my_first_instance.tf
@@ -3,8 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_first_instance" {
-  ami = ""
-  instance_type = ""
-  availability_zone = ""
+  ami = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1"
+  key_name = "fixed_keys"
+  vpc_security_group_ids = ["sg-0058243c39be91b77"]
+
+  tags = {
+    Name = "my_first_instance"
+  }
   
 }
