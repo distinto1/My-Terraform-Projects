314a1c4a0edefc0bf57111fcbb73312fa6ce15d2
distinto1 <ugot4daniel@gmail.com>
2024-06-11 15:52:08 +0100

    ran some terraform commands

--- Creating_simple_instance/my_first_instance.tf
+++ Creating_simple_instance/my_first_instance.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_first_instance" {
-  ami = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
-  availability_zone = "us-east-1"
-  key_name = "fixed_keys"
+  ami                    = "ami-04b70fa74e45c3917"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = "fixed_keys"
   vpc_security_group_ids = ["sg-0058243c39be91b77"]
 
   tags = {
     Name = "my_first_instance"
   }
-  
+
 }
