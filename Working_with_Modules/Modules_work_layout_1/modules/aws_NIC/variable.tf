5ff9cbb7188da2edddda39da80a5f190b41f6825
distinto1 <ugot4daniel@gmail.com>
2024-07-09 13:45:28 +0100

    add the variables for aws_nic in modules/aws_nic/variable.tf

---
+++ Working_with_Modules/Modules_work_layout_1/modules/aws_NIC/variable.tf
@@ -0,0 +1,17 @@
+variable "subnet_id" {
+  
+  type = string 
+}
+
+variable "private_ips" {
+  type = list(string)
+  default = ["172.16.10.100"] 
+}
+
+variable "network_interface_nametag" {
+  type = map(any)
+  default = {
+    "Name" = "primary_network_interface"
+  }
+  
+}
