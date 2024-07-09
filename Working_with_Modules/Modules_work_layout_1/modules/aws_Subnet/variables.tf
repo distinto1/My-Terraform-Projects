462e995b7e3975def86476ecf4bc41e40c34d90d
distinto1 <ugot4daniel@gmail.com>
2024-07-09 13:47:30 +0100

    add the variables for aws_subnet in modules/aws_subnet/variables.tf

---
+++ Working_with_Modules/Modules_work_layout_1/modules/aws_Subnet/variables.tf
@@ -0,0 +1,16 @@
+variable "subnet_cidr" {
+  type = string
+  default = "172.16.0.0/24"
+}
+
+variable "subnet_nametag" {
+  type = map(any)
+  default = {
+    "Name" = "sprint_july06"
+  }
+}
+
+variable "vpc_id" {
+  type = string
+  
+}
